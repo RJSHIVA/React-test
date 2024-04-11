efebae35424b89822850c246cc75cb2998624268
RAHUL CHOUDHARY <rc725383@gmail.com>
2024-04-11 16:58:14 +0530

    header updated

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -8,7 +8,7 @@ const Header = () => {
     <div className="bg-white shadow-md">
       <div className="container mx-auto px-4 py-2 flex justify-between items-center">
         {/* Logo */}
-        <a href="/" className="text-white hover:text-gray-900">
+        <a href="/" className="text-black hover:text-gray-900">
   Karo Abhyaas
 </a>
 
